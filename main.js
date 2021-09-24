d70853d40d3308ea29b69d7d102c2f8385bb1a28
Mac Rowe <m.rowe42@gmail.com>
2021-09-24 13:18:16 -0400

    Inital commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function insertDashes(str) {
+    // write code here
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        // arrange
+        const value = "aba caba";
+        
+        // act
+        const result = insertDashes(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe("a-b-a c-a-b-a");
+    });
+});
