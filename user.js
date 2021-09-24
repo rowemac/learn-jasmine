488369734835587af6bb6e73449ba57498ed1791
Mac Rowe <m.rowe42@gmail.com>
2021-09-24 14:22:47 -0400

    Add beforeEach to demonstrate random tests

--- user.js
+++ user.js
@@ -6,6 +6,6 @@ class User {
     constructor(data = {}) {
         this.firstName = data.firstName || '';
         this.lastName = data.lastName || '';
-        this.middleName = data.middleName;
+        this.middleName = data.middleName || '';
     }
 }
