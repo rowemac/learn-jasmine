9b404956e9a90569c4cfc0306ab34132f7a34875
Mac Rowe <m.rowe42@gmail.com>
2021-09-24 14:49:58 -0400

    Add get fullName function to User class

--- user.js
+++ user.js
@@ -8,4 +8,12 @@ class User {
         this.lastName = data.lastName || '';
         this.middleName = data.middleName || '';
     }
+
+    get fullName() {
+        if(this.middleName.length > 0) {
+            return `${this.firstName} ${this.middleName}. ${this.lastName}`;
+        }
+
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
